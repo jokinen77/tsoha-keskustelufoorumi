be9faf551baef1f13c5879566d96dea46d7d769b
Jaakko Jokinen <jaakkojokinen@icloud.com>
2019-01-20 16:26:51 +0200

    postgreSQL implementation

--- application/__init__.py
+++ application/__init__.py
@@ -1,26 +1,40 @@
-# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__, static_url_path='')
 
-# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
-# Käytetään forum.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
-app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
 from application import views
-
 from application.forums import models
-
 from application.auth import models
 
+# login
+# from application.auth.models import User
+# from os import urandom
+# app.config["SECRET_KEY"] = urandom(32)
+#
+# from flask_login import LoginManager
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+#
+# login_manager.login_view = "auth_login"
+# login_manager.login_message = "Please login to use this functionality."
+#
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(user_id)
+#
 
-# Luodaan lopulta tarvittavat tietokantataulut
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
